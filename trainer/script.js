786979861a2e3dc8de5bd7ec3c92d9dc3943c8fc
Or Bibi <orbibiariel@gmail.com>
2025-12-07 21:51:46 +0200

    Delete trainer directory

--- trainer/script.js
+++
@@ -1,4 +0,0 @@
-console.log('Personal Fitness Coach Page Loaded');
-// The lead form submission is handled by the system as per the Ironclad Protocol.
-// No client-side JavaScript is needed or allowed for form submission logic.
-// Any future animations or UI logic would go here.
